8999e12b33a5cd54204e1bf9d21836db9c058a40
AndrysDM <andrysmoreno4@gmail.com>
2024-04-28 21:09:48 -0400

    Se ven los pokemonesdescalificados

--- torneoinscripcion.py
+++ torneoinscripcion.py
@@ -15,11 +15,18 @@ def torneoinscripciones(inscritos,descalificados):
                 st.image(fotos[i],caption=dia[1][i][1])
     total = len(descalificados[0]) + descalificados[1]
     descalificados=descalificados[0]
-    st.markdown(
-        f"""
-        <div style='background-color: #ffffff; padding: 10px; border-radius: 10px;'>
-            <h3 style='text-align: center; color: #333;'>Se han inscrito al torneo <span style='color: red;'>{total}</span> Pokemones de los cuales se han descalificado <span style='color: red;'>{len(descalificados)}</span></h3>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )
+    if(len(descalificados)>0):
+        st.markdown(
+            f"""
+            <div style='background-color: #ffffff; padding: 10px; border-radius: 10px;'>
+                <h3 style='text-align: center; color: #333;'>Se han inscrito al torneo <span style='color: red;'>{total}</span> Pokemones de los cuales se han descalificado <span style='color: red;'>{len(descalificados)}</span></h3>
+            </div>
+            """,
+            unsafe_allow_html=True
+        )
+        for i in range(len(descalificados)):
+            if i%7==0:
+                col = st.columns(7)
+            with col[i%7]:
+                st.image(descalificados[i][0])
+        
